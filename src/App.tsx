06c7640bbeb7d658d17a679bf961f757d62a7a04
Maydelith Laura Zuñiga Cabrera <maydelith.zuniga@utec.edu.pe>
2026-09-12 18:13:32 -0500

    Implement routing for Home, Category, Dashboard, and NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import Home from "./pages/Home";
+import Category from "./pages/Category";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
 
 function App(): React.JSX.Element {
   return (
     <>
       <Header />
       <main>
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/categoria/:categoryId" element={<Category />} />
+          <Route path="/analitica" element={<Dashboard />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </main>
     </>
   );
