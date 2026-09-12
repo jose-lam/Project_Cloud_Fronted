9604d2f4a2b23d70fb91a31209903d80ec2c10e5
jose-lam <jose.ruiz@utec.edu.pe>
2026-09-12 17:07:04 -0500

    fix: agregar dependencia react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Header } from './components/Header';
-import { Home } from './pages/Home';
+import Home from "./pages/Home";
 
 function App(): React.JSX.Element {
   return (
