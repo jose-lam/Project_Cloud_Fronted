726ea52d017f073d96d7a8290f501af41d5926a8
Maydelith Laura Zuñiga Cabrera <maydelith.zuniga@utec.edu.pe>
2026-09-12 19:08:06 -0500

    Reorder import of layout.css in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
-
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './layout.css'
 import App from './App.tsx'
+import './layout.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
